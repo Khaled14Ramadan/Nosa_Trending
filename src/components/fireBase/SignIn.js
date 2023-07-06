c63005922ec3b90afb3f97b3cf44e66e8416fb09
Khaled Ramadan <50340510+Khaled14Ramadan@users.noreply.github.com>
2023-07-06 15:43:12 +0200

    update SignIn and SignUp

---
+++ src/components/fireBase/SignIn.js
@@ -0,0 +1,25 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from "./FireBaseConfig";
+const auth = getAuth(app);
+const SignIn = (email, password) => {
+  return new Promise((success, error) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("user : ", user);
+        alert("succsefully SigIn");
+        success(true);
+        // ...
+      })
+      .catch((e) => {
+        const errorCode = e.code;
+        const errorMessage = e.message;
+        console.log("error signin : ", errorCode);
+        error(e);
+        // ..
+      });
+  });
+};
+
+export default SignIn;
