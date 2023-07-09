8f427889dfded533d4cd04ddd0157cb95c55b34e
Khaled Ramadan <50340510+Khaled14Ramadan@users.noreply.github.com>
2023-07-09 12:35:35 +0200

    Revert "update SignIn and SignUp"
    This reverts commit c63005922ec3b90afb3f97b3cf44e66e8416fb09.

--- src/App.js
+++ src/App.js
@@ -1,112 +1,85 @@
-import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
-import { Route, Routes, useNavigate, Navigate } from "react-router-dom";
-import Movie from "./components/Movies/Movie";
-import TvShow from "./components/TvShow/TvShow";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import Notfound from "./components/Notfound/Notfound";
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home';
+import { Route, Routes ,useNavigate ,Navigate } from "react-router-dom";
+import Movie from './components/Movies/Movie';
+import TvShow from './components/TvShow/TvShow';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Notfound from './components/Notfound/Notfound';
 
-import { useState, useEffect } from "react";
-import jwtDecode from "jwt-decode";
-import Footer from "./components/Footer/Footer";
-import MovieDetails from "./components/MovieDetails/MovieDetails";
-import TvDetails from "./components/TvDetails/TvDetails";
-import SignUpForm from "./components/SignUpForm/SignUpForm";
-import SignInForm from "./components/SignInForm/SignInForm";
-import SignOut from "./components/fireBase/SignOut";
-import "./App.css";
+import { useState , useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
+import Footer from './components/Footer/Footer';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import TvDetails from './components/TvDetails/TvDetails';
 
 export default function App() {
-  let navigate = useNavigate();
-  const [userData, setUserData] = useState(null);
 
-  useEffect(() => {
-    if (localStorage.getItem("userToken")) {
-      getUserData();
+    let navigate = useNavigate();
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+     if(localStorage.getItem('userToken')){
+        getUserData();
+     }
+    }, []);
+
+    useEffect(() => {
+        console.log(userData);
+       }, [userData]);
+    
+
+    let getUserData = ()=>
+    {
+        let decodedToken = jwtDecode(localStorage.getItem("userToken"));
+        setUserData(decodedToken);
+        console.log("tsts" ,decodedToken);
+       // console.log(userData);
     }
-  }, []);
 
-  useEffect(() => {
-    console.log(userData);
-  }, [userData]);
-
-  let getUserData = () => {
-    let decodedToken = jwtDecode(localStorage.getItem("userToken"));
-    setUserData(decodedToken);
-    console.log("tsts", decodedToken);
-    // console.log(userData);
-  };
-
-  let logOut = () => {
-    SignOut();
-    localStorage.removeItem("userToken");
-    setUserData(null);
-    console.log("helloooooo");
-    navigate("/login");
-  };
-
-  function ProtectedRoute({ children }) {
-    if (!localStorage.getItem("userToken")) {
-      return <Navigate to="/login" />;
-    } else {
-      return children;
+    let logOut=()=>
+    {
+        localStorage.removeItem("userToken");
+        setUserData(null);
+        console.log("helloooooo");
+        navigate('/login');
     }
-  }
+
+    function ProtectedRoute({children})
+    {
+        if(!localStorage.getItem('userToken'))
+        {
+            return <Navigate to='/login'/>
+        }
+        else
+        {
+            return children;
+        }
+    }
 
   return (
     <>
-      <Navbar logOut={logOut} />
-      <div className="container">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />{" "}
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="home"
-            element={
-              <ProtectedRoute>
-                <Home />{" "}
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="movies"
-            element={
-              <ProtectedRoute>
-                <Movie />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="tvshow"
-            element={
-              <ProtectedRoute>
-                <TvShow />
-              </ProtectedRoute>
-            }
-          />
-          {/* <Route path="login" element={<Login />} /> */}
-          <Route path="login" element={<SignInForm />} />
-          {/* <Route path="register" element={<Register />} /> */}
-          <Route path="register" element={<SignUpForm />} />
-          <Route path="*" element={<Notfound />} />
+       <Navbar logOut={logOut}/>
+       <div className='container'>
+            <Routes>
+                <Route path='/' element={<ProtectedRoute><Home/> </ProtectedRoute>} />
+                <Route path='home' element={<ProtectedRoute><Home/> </ProtectedRoute>} />
+                <Route path='movies' element={<ProtectedRoute><Movie/></ProtectedRoute>} />
+                <Route path='tvshow' element={<ProtectedRoute><TvShow/></ProtectedRoute> }/>
+                <Route path='login' element={<Login/> }/>
+                <Route path='register' element={<Register/> }/>
+                <Route path='*'   element={<Notfound/>}/>
 
-          <Route path="moviedetails" element={<MovieDetails />}>
-            <Route path=":id" element={<MovieDetails />} />
-          </Route>
-          <Route path="tvdetails" element={<TvDetails />}>
-            <Route path=":id" element={<TvDetails />} />
-          </Route>
-        </Routes>
-      </div>
-      <Footer />
+                <Route path='moviedetails' element ={<MovieDetails/> } >
+                    <Route path=':id'  element ={<MovieDetails/> } />
+                </Route>
+                <Route path='tvdetails' element ={<TvDetails/> } >
+                    <Route path=':id'  element ={<TvDetails/> } />
+                </Route>
+            </Routes>
+       </div>
+       <Footer/>
     </>
-  );
+  )
 }
